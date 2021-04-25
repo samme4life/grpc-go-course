5c548db0958b0ff9e4ff795dc35eae2e61b72168
samme <samme4life@gmail.com>
2021-04-26 09:48:48 +1000

    L17 Server Setup Boilerplate Code

--- generate_code.sh
+++ generate_code.sh
@@ -1,1 +1,9 @@
-protoc -I greet/ --go_out=greet/greetpb greet/greetpb/greet.proto
+#L16 Code Generation Test
+# This doesn't work anymore.
+#refer https://grpc.io/docs/languages/go/quickstart/#regenerate-grpc-code for the up-to-date command
+protc greet/greetpb/greet.proto --go_out=plugins=grpc:.
+
+#This command works just fine
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    greet/greetpb/greet.proto
