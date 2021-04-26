f736adba2bf600402426d543147bef53b09068f8
samme <samme4life@gmail.com>
2021-04-26 21:22:52 +1000

    L 20, 21 & 22 Greet Unary API implementation

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/samme4life/grpc-go-course/greet/greetpb"
 	"google.golang.org/grpc"
@@ -18,6 +19,21 @@ func main() {
 	defer cc.Close()
 
 	c := greetpb.NewGreetServiceClient(cc)
-	fmt.Printf("Created client: %f", c)
 
+	doUnary(c)
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do Unary RPC...")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Samme",
+			LastName:  "Samarasinghe",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling Greet RPC: %v", err)
+	}
+	log.Printf("Response from the Greet RPC: %v", res.Result)
 }
--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/samme4life/grpc-go-course/greet/greetpb"
 	"log"
@@ -13,8 +14,19 @@ type server struct {
 	greetpb.UnimplementedGreetServiceServer
 }
 
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Printf("Greet function was invoked with %v\n", req)
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstName
+
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
+
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Server Started!")
 
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
