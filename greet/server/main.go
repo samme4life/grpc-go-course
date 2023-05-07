cf65914c6a92c29367fe4a1b14a04c11e022232c
ssamarasin <samme.samarasinghe@comparethemarket.com.au>
2023-05-07 22:59:42 +1000

    L17 Server Setup Boilerplate Code

---
+++ greet/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"log"
+	"net"
+
+	pb "github.com/samme4life/grpc-go-course/greet/proto"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatalf("Failed to listen on: %v\n", err)
+	}
+
+	log.Printf("Listening on: %s\n", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
+}
