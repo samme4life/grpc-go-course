9ccebda3f646016d71846d84d9a7ba0929f1816d
ssamarasin <samme.samarasinghe@comparethemarket.com.au>
2023-05-08 07:53:19 +1000

    L21 Unary API Server Implementation

--- greet/server/main.go
+++ greet/server/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"google.golang.org/grpc"
+	pb "grpc-go-course/greet/proto"
 	"log"
 	"net"
 )
 
 var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
 
 func main() {
 	lis, err := net.Listen("tcp", addr)
@@ -18,6 +23,7 @@ func main() {
 	log.Printf("Listening on: %s\n", addr)
 
 	s := grpc.NewServer()
+	pb.RegisterGreetServiceServer(s, &Server{})
 
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v\n", err)
